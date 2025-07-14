d47fd84a3117db5425295a1cbd1274ca5f7144f5
Jhon Bocanegra <jhon.bocanegra26@gmail.com>
2025-07-14 12:29:51 -0500

    feat: add dashboard, trending, and search pages with routing
    - Updated app.routes.ts to include routes for dashboard, trending, and search components.
    - Created dashboard component with HTML layout and basic styling.
    - Created search component with a simple placeholder message.
    - Created trending component with a simple placeholder message.
    - Updated global styles to include Tailwind CSS.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard/dashboard').then((c) => c.Dashboard),
+  },
+  {
+    path: 'trending',
+    loadComponent: () =>
+      import('./gifs/pages/trending/trending').then((c) => c.Trending),
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./gifs/pages/search/search').then((c) => c.Search),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
---
+++ src/app/gifs/pages/trending/trending.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending',
+  imports: [],
+  templateUrl: './trending.html',
+  styles: ``
+})
+export class Trending {
+
+}
